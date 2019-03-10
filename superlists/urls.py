415e090ac444db602c753a2014523f2bad09622e
Tisipi <Tisipi@tisipi.com>
2019-03-10 19:20:52 +0100

    Migration to Mint

--- superlists/urls.py
+++ superlists/urls.py
@@ -13,17 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-
-# Not needed now:
-#   from django.contrib import admin
-
-#   urlpatterns = [
-#       url(r'^admin/', admin.site.urls),
-#   ]
-
 from django.conf.urls import url
-from lists import views
+from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', views.home_page, name='home')
+    url(r'^admin/', admin.site.urls),
 ]
