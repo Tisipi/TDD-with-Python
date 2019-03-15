d42c08f19cf1d171922204d01f3da1a663aa70a5
Tisipi <ericbrouwers@home.nl>
2019-03-15 17:09:29 +0100

    Added url mapping with dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,6 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
 # Create your tests here.
 
@@ -6,3 +8,10 @@ class SmokeTest(TestCase):
 
     def test_bad_maths(self):
         self.assertEqual(1+1, 2)
+
+
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
