092f8275646ec82ed4447959ad39a984a05a0389
Tisipi <tisipi@tisipi.nl>
2022-03-21 20:06:17 +0100

    Front page HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -18,17 +18,18 @@ class NewVisitorTest(unittest.TestCase):
         # to check out its homepage
         self.browser.get("http://localhost:8000")
 
-        # She notices the page title and header mention to-do lists
-        self.assertIn("To-Do", self.browser.title)
+        # She notices the page title and header mention To-Do Lists
+        self.assertIn("To-Do List", self.browser.title)
         header_text = self.browser.find_element_by_tag_name("h1").text
-        self.assertIn("To-Do", header_text)
+        self.assertIn("To-Do List", header_text)
 
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id("id_new_item")
         self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
+        time.sleep(1)
 
-        # She types "Buy peacock feathers" into a text box (Edith's hobby
-        # is tying fly-fishing lures)
+        # She types "Buy peacock feathers" into a text box
+        # (Edith's hobby is tying fly-fishing lures)
         inputbox.send_keys("Buy peacock feathers")
 
         # When she hits enter, the page updates, and now the page lists
@@ -38,7 +39,10 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id("id_list_table")
         rows = table.find_elements_by_tag_name("tr")
-        self.assertTrue(any(row.text == "1: Buy peacock feathers" for row in rows))
+        self.assertTrue(
+            any(row.text == "1: Buy peacock feathers" for row in rows),
+            "New to-do item did not appear in table",
+        )
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
